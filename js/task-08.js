0679da79de63d3ab5885e142f05a9c285731d6b0
taraspro5 <kovtun_taras007@ukr.net>
2023-07-27 20:37:38 +0300

    Update task-08.js

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,17 @@
+const elements = {
+    form: document.querySelector('.login-form'),
+    email: document.querySelector('.email'),
+    password: document.querySelector('.password')
+}
 
+elements.form.addEventListener('submit', handlerForm)
+
+function handlerForm(evt){
+    evt.preventDefault();
+    const {email, password} = evt.currentTarget.elements;
+    if(email.value === '' || password.value === ''){
+        return alert("All fields must be filled!")
+    }
+    console.log(`Email: ${email.value}, password: ${password.value}`);
+    evt.currentTarget.reset();
+}
