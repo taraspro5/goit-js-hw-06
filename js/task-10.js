3a0c13011d9fba27458c4a509cefe811769b3d88
taraspro5 <kovtun_taras007@ukr.net>
2023-07-27 21:26:47 +0300

    Update task-10.js

--- js/task-10.js
+++ js/task-10.js
@@ -7,6 +7,29 @@ function getRandomHexColor() {
 const elements = {
   input: document.querySelector('input'),
   btnCreate: document.querySelector('[data-create]'),
-  btnDestroy: document.querySelector('[data-destroy]')
+  btnDestroy: document.querySelector('[data-destroy]'),
+  box: document.querySelector('#boxes')
 }
 
+elements.btnCreate.addEventListener('click', createBoxes)
+elements.btnDestroy.addEventListener('click', destroyBoxes)
+
+function createBoxes(amount){
+  amount = elements.input.value;
+  let arrBoxes = [];
+  let boxSize = 30;
+  for(let i = 0; i < amount; i++){
+    const randomColor = getRandomHexColor();
+    const box = `<div style="width:${boxSize}px; height:${boxSize}px; background-color:${randomColor}"></div>`;
+    boxSize+=10;
+    arrBoxes.push(box);
+  }
+  // arrBox.join('')
+  const boxes = arrBoxes.join('');
+  elements.box.innerHTML = boxes;
+}
+
+function destroyBoxes(){
+  elements.box.innerHTML = '';
+  elements.input.value = 0;
+}
